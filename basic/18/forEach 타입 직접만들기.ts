a05dcc1c11f15e12a5a15ace46b1bf02ae9f0fc7
minsoo.lee <letomok@gmail.com>
2022-09-08 09:59:36 +0900

    forEach 타입 만들기

---
+++ basic/18/forEach 타입 직접만들기.ts
@@ -0,0 +1,32 @@
+(function () {
+    interface Arr<T> {
+        forEach(callback: (item: T) => void): void
+    }
+
+
+    const a: Arr<number> = [1, 2, 3]
+
+    a.forEach((item) => {
+        console.log(item)
+        item.toFixed(1)
+    })
+
+    a.forEach((item) => {
+        console.log(item)
+        return 3
+    })
+
+    const b: Arr<string> = ['1', '2', '3']
+
+    b.forEach((item) => {
+        console.log(item)
+        item.charAt(3)
+    })
+
+    b.forEach((item) => {
+        console.log(item)
+        return 3
+    })
+
+})();
+
