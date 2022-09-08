083731edcdacbc1cf1e153197bfb391025aa0801
minsoo.lee <letomok@gmail.com>
2022-09-08 11:07:05 +0900

    오버로딩

---
+++ basic/22/타입overloading.ts
@@ -0,0 +1,7 @@
+/**
+ * 타입 오버로딩
+ */
+
+declare function add(x: number, y: string): number
+declare function add(x: string, y: string): string
+declare function add(x: number, y: string): number | string
